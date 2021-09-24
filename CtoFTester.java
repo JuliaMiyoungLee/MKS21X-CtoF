0f780eab817cc7004ca1a5f0962d23adbc647ea8
Julia Lee <jlee30@stuy.edu>
2021-09-23 22:22:09 -0400

    Added comment

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,4 @@
-ECHO is on.
+/*I would need the parameters that give the starting degrees value and then one that indicates whether that value is in F or C.
+The degrees would be a double and whether the value is F or C I would make a char.
+I would prompt the person to type either 'F' or 'C' depending on whether its Farenheit or celcius
+I would have the functions return a double and then round the double later when I print it*/
